7507580ca419674454633c7133563ef3d063fea1
Julian Hoyos <jhoyos135@gmail.com>
2019-03-22 19:37:40 -0400

    finish ajax calls

--- public/assets/scripts.js
+++ public/assets/scripts.js
@@ -1,46 +1,76 @@
-$(() => {
-    $('.devourUndevour').on('click', function(event) {
-      let toggleDevoured = {
-        devoured: Math.abs($(this).data('devoured') - 1)
-      };
-      $.ajax(`/api/update-burger/${$(this).data('id')}`, {
-        type: 'PUT',
-        data: toggleDevoured
-      }).then(
-        () => {
-          console.log(`Updated devoured state to ${toggleDevoured}`);
-          location.reload();
-        }
-      );
-    });
-  
-    $('.burgerForm').on('submit', (event) => {
-      event.preventDefault();
-      let burger_name = $('#burger_name').val().trim();
-      if (burger_name !== '') {
-        $.ajax('/api/new-burger', {
-          type: 'POST',
-          data: {
-            burger_name: burger_name,
-            devoured: $('[name=devoured]:checked').val()
-          }
-        }).then(
-          () => {
-            console.log(`created new burger: ${burger_name}`);
+document.addEventListener("DOMContentLoaded", () => {
+
+// add burger
+document.querySelector('.burgerForm').addEventListener('submit', (e) => {
+    e.preventDefault();
+    let burger_name = document.querySelector('#burger_name').value;
+    let url = `/api/new-burger`;
+    if(burger_name !== '') {
+        //ajax call
+        let fetch_burger = async () => {
+            let devaour_val = document.querySelector('[name=devoured]:checked').value
+            let res = await fetch(url, {
+                
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'   
+                },
+                body: JSON.stringify({
+                    burger_name: burger_name,
+                    devoured: devaour_val
+                })
+            })
+            let json = await res.json();
+            console.log(json)
             location.reload();
-          }
-        );
-      }
-    });
-  
-    $(".deleteBurger").on("click", function(event) {
-      let id = $(this).data("id");
-      $.ajax("/api/delete-burger/" + id, {
-        type: "DELETE"
-      }).then( () => {
-          console.log("deleted burger", id);
-          location.reload();
         }
-      );
-    });
-  });
+        fetch_burger()
+    }
+})
+
+// delete burger
+let delete_btn = document.querySelectorAll('.deleteBurger');
+delete_btn.forEach((single_delete) => {
+    single_delete.addEventListener('click', (e) => {
+        let target = e.target.dataset.id;
+       
+        let delete_fetch = async () => {
+            let url = `/api/delete-burger/${target}`;
+            await fetch(url, {
+                method: 'DELETE',
+            })
+            location.reload();
+        }
+        delete_fetch();
+    })
+})
+
+// update the burger status
+let devourUndevour = document.querySelectorAll('.devourUndevour');
+devourUndevour.forEach((single_devour) => {
+    single_devour.addEventListener('click', (e) => {
+        let target =  e.target.dataset.devoured;
+        let id =  e.target.dataset.id;
+        let toggle = Math.abs(target -  1);
+        
+        // ajax call
+        let update_fetch = async () => {
+            let url = `/api/update-burger/${id}`
+        await fetch(url, {
+                method: 'PUT',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'   
+                },
+                body: JSON.stringify({
+                    devoured: toggle
+                })
+                
+            })
+            location.reload()
+        }
+        update_fetch()
+    })
+})
+});
