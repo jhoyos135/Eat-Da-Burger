cf6394d0ab3d61328d2f351d98b0b5139000b619
Julian Hoyos <jhoyos135@gmail.com>
2019-03-22 21:11:23 -0400

    more styles

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 80;
 
 const app = express();
 
